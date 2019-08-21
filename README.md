66ef4d7bbd1ce2fe7fa6c8b6016786d65f47f41c
Manuel Ospina <manuel@anect.jp>
2019-08-21 14:51:58 +0900

    Add flow for typechecks

--- README.md
+++ README.md
@@ -22,6 +22,8 @@ For unit test
 > npm install --save-dev enzyme react-addons-test-utils enzyme-adapter-react-16
 
 For type checks
+> npm install --save-dev flow-bin
+> node_modules/flow-bin/flow-linux64-v0.105.2/flow init
 > npm install prop-types
 
 ## Available Scripts
@@ -32,3 +34,11 @@ In the project directory, you can run:
 - `npm test` : Launches the test runner
 - `npm run build` : builds the app for production to the `build` folder
 - `npm run eject` : **Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+ 1058  npm run flow init
+ 1059  npm run node_modules/flow-bin/flow-linux64-v0.105.2/flow init
+ 1061  ls
+ 1062  ls -a
+ 1063  history | tail -n 5 
+ 1064  history | tail -n 10 
+ 1065  history | tail -n 10 >> README.md 
--- src/model/Entry.js
+++ src/model/Entry.js
@@ -2,21 +2,22 @@ class Entry {
 
   /*
    * date : Date
-   * time : Date
    * label : String
    * Reminder: Bool
-   * intervalOfRepetition : Time delta
+   * intervalOfRepetition : String
   */
 
   // REQUIRES: date and time are in ISO format 'YYYY-MM-DD' 'HH:MM'
   // MODIFIES: this
   // EFFECTS: Set date and time during initialization
-  constructor(date, time){
+  // @flow
+  constructor(date: string, time: string): void{
     this._date = new Date(date + 'T' + time);
   };
 
   // EFFECTS: returns date without time
-  get date(){
+  // @flow
+  get date(): string{
     let year = this._date.getFullYear();
     let month = this._date.getMonth() + 1;
     let day = this._date.getDate();
@@ -25,7 +26,8 @@ class Entry {
   };
 
   // EFFECTS: returns time without date
-  get time(){
+  // @flow
+  get time(): string{
     let hours = this._date.getHours();
     let minutes = this._date.getMinutes();
     
@@ -33,24 +35,28 @@ class Entry {
   };
 
   // EFFECTS: returns label
-  get label(){
+  // @flow
+  get label(): string{
     return this._label;
   };
 
   // EFFECTS: returns interval
-  get interval(){
+  // @flow
+  get interval(): string{
     return this._interval;
   };
 
   // EFFECTS: returns reminder
-  get reminder(){
+  // @flow
+  get reminder(): boolean{
     return this._reminder;
   };
 
   // REQUIRES: date to be in ISO format 'YYYY-MM-DD'
   // MODIFIES: this
   // EFFECTS: set date, using existing time or 00:00
-  set date(date){
+  // @flow
+  set date(date: string): void{
     let time = this.time
     this._date = new Date(date + 'T' + time);
   };
@@ -58,29 +64,36 @@ class Entry {
   // REQUIRES: time to be in ISO format 'HH:MM:SS'
   // MODIFIES: this
   // EFFECTS: set date using date from existing date or now.
-  set time(time){
+  // @flow
+  set time(time: string): void{
     let date = this.date
     this._date = new Date(date + 'T' + time);
   };
 
   // MODIFIES: this
-  set label(label){
+  // @flow
+  set label(label: string): void{
     this._label = label;
   };
 
   // REQUIRES: interval to be one of (daily, weekly, monthly, annually)
   // MODIFIES: this
   // EFFECTS: set interval of repetition.
-  set interval(interval){
+  // type Interval = 'daily' | 'weekly' | 'monthly' | 'annually'
+  // @flow
+  set interval(interval: string): void{
     this._interval = interval;
   };
 
   // EFFECTS: returns reminder
-  set reminder(reminder){
+  // @flow
+  set reminder(reminder: boolean): void{
     this._reminder = reminder;
   };
 
-  leadingZero(number){
+  // EFFECTS: returns a string with a leading zero if number is one single digit
+  // @flow
+  leadingZero(number: number): string{
     let n = number.toString();
     if (n.length == 1) {
       return "0" + n;
