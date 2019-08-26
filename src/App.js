67f4a5b85bf53f5949012a9f7e01c386fdb32856
Manuel Ospina <manuel@anect.jp>
2019-08-26 15:05:21 +0900

    Add EmailForm to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import {EmailForm} from './components/EmailForm'
+import {Calendar} from './model/Calendar'
 
 function DateTime() {
   const today = new Date();
@@ -9,13 +11,49 @@ function DateTime() {
   )
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Calendar</h2>
-      <DateTime />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      calendar: null
+    };
+    
+    this.onEmailFormChange = this.onEmailFormChange.bind(this);
+    this.onEmailFormSubmit = this.onEmailFormSubmit.bind(this);
+  }
+
+  onEmailFormChange(event){
+    this.setState({email: event.target.value});
+  }
+
+  onEmailFormSubmit(event){
+    const {email} = this.state;
+    this.setState({calendar: new Calendar(email)});
+
+    event.preventDefault();
+  };
+
+  render(){
+    const { email, calendar } = this.state;
+
+    return (
+      <div className="App">
+        <h2>Calendar</h2>
+        { calendar
+          ? <DateTime />
+          : <EmailForm
+              value={email}
+              onChange={this.onEmailFormChange}
+              onSubmit={this.onEmailFormSubmit}
+            >
+              Set email
+            </EmailForm>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
