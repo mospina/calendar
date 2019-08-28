fd687e8045a4c70d68bdb82bb7b54ed3f2d3b174
Manuel Ospina <manuel@anect.jp>
2019-08-28 14:21:12 +0900

    Add nav buttons to add entries

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -3,23 +3,74 @@
 import React, { Component } from 'react';
 import {Entry} from '../model/Entry';
 
+type EntryType = null | 'event' | 'meeting' | 'reminder';
+
 type Props = {
   addEntry : (Entry) => void
 }
 
-class EntryForm extends Component<Props> {
+type State = {
+  entryType : ?EntryType
+}
+
+class EntryForm extends Component<Props, State> {
+
+  constructor(props: Props){
+    super(props);
+    this.state = {
+      entryType: null
+    }
+  }
 
   runAddEntry = (fn: (Entry)=>void) => {
     const entry = new Entry('2019-08-28', '10:00');
     fn(entry);
   }
 
+  modifyEntryType = (event: SyntheticEvent<HTMLButtonElement>) => {
+    this.setState({entryType: event.currentTarget.value});
+
+    event.preventDefault();
+  }
+
+  selectFormComponent = (entryType: EntryType) => {
+    switch (entryType) {
+    case 'event':
+      return (<EventForm />);
+    case 'meeting':
+      return (<MeetingForm />);
+    case 'reminder':
+      return (<ReminderForm />);
+    default:
+      return (<div></div>);
+    }
+  }
+
   render(){
     const {addEntry} = this.props;
+    const {entryType} = this.state;
 
     return (
       <div className="entry-form">
         <div className="form-menu">
+          <button 
+            value="event"
+            onClick={this.modifyEntryType}
+            type="button">
+              Add Event
+          </button>
+          <button 
+            value="meeting"
+            onClick={this.modifyEntryType}
+            type="button">
+              Add Meeting
+          </button>
+          <button 
+            value="reminder"
+            onClick={this.modifyEntryType}
+            type="button">
+              Add Reminder
+          </button>
           <button 
             onClick={() => this.runAddEntry(addEntry)}
             type="button">
@@ -27,10 +78,15 @@ class EntryForm extends Component<Props> {
           </button>
         </div>
         <div className="form">
+        { this.selectFormComponent(entryType) }
         </div>
       </div>
     );
   }; 
 }
+
+const EventForm = (props) => (<div>Event</div>)
+const MeetingForm = (props) => (<div>Meeting</div>)
+const ReminderForm = (props) => (<div>Reminder</div>)
 
 export {EntryForm};
