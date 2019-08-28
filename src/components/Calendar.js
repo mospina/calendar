735d3539ed0d8e38c830634e6b237bd3f9bb805d
Manuel Ospina <manuel@anect.jp>
2019-08-28 09:25:46 +0900

    Add divs to Calendar component.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,8 @@
 // @flow
 
 import React, { Component } from 'react';
+import {EntryList} from './EntryList';
+import {EntryForm} from './EntryForm';
 import {Calendar} from '../model/Calendar';
 
 type Props = {
@@ -13,7 +15,20 @@ class CalendarWidget extends Component<Props> {
     const {calendar} = this.props;
 
     return (
-      <h3>{calendar.date + ' ' + calendar.time}</h3>
+      <div id="calendar">
+        <h2>{calendar.date + ' ' + calendar.time}</h2>
+        <div className="add-entry">
+          <EntryForm addEntry={(entry) => calendar.addEntry(entry)} />
+        </div>
+        <div className="entry-list">
+          <h3>Today</h3>
+          <EntryList entries={calendar.getTodayEntries()} />
+        </div>
+          <h3>This week</h3>
+          <EntryList entries={calendar.getThisWeekEntries()} />
+        <div className="entry-list">
+        </div>
+      </div>
     )
   }
 }
